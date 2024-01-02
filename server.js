9bc1f5864936e5e42c6ea486d42e92207ad4933d
Leandroni1983 <leandronicolas1983@gmail.com>
2024-01-02 13:16:51 -0300

    fix: cors origin

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 3000;
 
 
 const corsOptions = {
-  origin: process.env.ORIGIN_URL || 'http://localhost:3001/',   
+  origin: process.env.CORS_ORIGIN,   
   optionsSuccessStatus: 200 // Algunas versiones de navegador/axios pueden requerir esta opción
 };
 
